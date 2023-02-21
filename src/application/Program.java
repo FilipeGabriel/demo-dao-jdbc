0ade205c905c038fda0f0232aa85b62b9ceacec1
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-02-21 19:40:38 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,13 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println();
+		
+		System.out.println("=== Teste 3: seller findAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
 	}
 
 }
