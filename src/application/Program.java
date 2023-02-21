3ebff7749e4bf5b2620a2e9b83d84609d341c93b
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-02-21 20:41:36 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,6 +43,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
+		System.out.println();
+		
+		System.out.println("=== Teste 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
